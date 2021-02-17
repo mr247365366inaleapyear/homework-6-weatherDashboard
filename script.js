0ab7ebb835f3fd952f28ad6042a068a4ecfe8c7a
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-17 12:57:24 -0500

    redoing logic

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 var city = $("#searchTerm").val();
 var apiKey = "&appid=48a5cb92f8bcd3cfcc83073fb0090b83";
 
-var date = new Date();
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
 
-function getItems() {
-  var cities = JSON.parse(localStorage.getItem("search"));
-  if (cities == null) {
-    search = cities;
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = mm+'-'+dd+'-'+yyyy;
+console.log(today);
+
+var oneDay = new Date;
+var oneDD = oneDay.getDate()+1;
+var mm = oneDay.getMonth()+1; 
+var yyyy = oneDay.getFullYear();
+if(oneDD<10) 
+{
+    oneDD='0'+oneDD;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = mm+'-'+dd+'-'+yyyy;
+console.log(oneDay);
+
+function currentWeather() {
+  var city = JSON.parse(localStorage.getItem("search"));
+  if (city == null) {
+    search = city;
   };
-  for (i = 0, i < search.length; i++) {
+  for (i = 0, i < search.length; i++;) {
     if (i == 8) {
       break;
     }
   }
+  //console.log(currentWeather);
+  console.log(fiveDayForcast);
 };
