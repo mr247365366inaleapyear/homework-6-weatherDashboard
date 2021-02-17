1ae784e37c031941352789f1867e8219d5051917
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-17 11:08:39 -0500

    started city search function for script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ var apiKey = "&appid=48a5cb92f8bcd3cfcc83073fb0090b83";
 
 var date = new Date();
 
+function getItems() {
+  var cities = JSON.parse(localStorage.getItem("search"));
+  if (cities == null) {
+    search = cities;
+  };
+  for (i = 0, i < search.length; i++) {
+    if (i == 8) {
+      break;
+    }
+  }
+};
